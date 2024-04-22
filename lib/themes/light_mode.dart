3b39699b60826bc4bbc5deeec87a30b622793ab9
shikhar <shikhars369@gmail.com>
2024-04-22 19:00:57 +0530

    test

--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
   colorScheme: const ColorScheme.light(
-      background: Color.fromRGBO(30, 37, 40, 1),
+      surface: Color.fromRGBO(30, 37, 40, 1),
     primary: Color.fromRGBO(0, 224, 145, 1),
     secondary: Colors.white,
     inversePrimary: Colors.black
